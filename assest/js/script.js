10ad13e0c5d2142df3caa9e65f1a168fb32f5d30
mahmoud-mohamed93 <mahmoud.gadadah@gmail.com>
2022-09-02 15:01:01 +0200

    changes

---
+++ assest/js/script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    items:2,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
